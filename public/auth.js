a91d86e85d07f203b96934ff3133767487052735
DreevGod <92431450+DreevGod@users.noreply.github.com>
2025-06-22 21:13:51 +0300

    Create public/auth.js

---
+++ public/auth.js
@@ -0,0 +1,8 @@
+<form id="authForm">
+  <input type="email" id="email" placeholder="Email" required />
+  <input type="password" id="password" placeholder="Пароль" required />
+  <button type="submit">Войти / Зарегистрироваться</button>
+</form>
+<div id="status"></div>
+
+<script type="module" src="auth.js"></script>
